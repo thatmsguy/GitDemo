3ae2770f6cf60317695204a7a1de213e0e87d7a6
thatmsguy <thatmscguy@gmail.com>
2020-05-23 19:18:44 +0100

    this is commit from GMT QA

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -3,11 +3,13 @@ package qaclickacademy;
 import org.testng.annotations.Test;
 //rahulonlinetutor@gmail.com
 public class RESTAPITest {
-
+//changes made here for git 2 new post jira added
 	@Test
 	public void postJira()
 	{
 		System.out.println("postJira");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
 	}
 	
 	@Test
